a59ae223802dddbe6a00e9268c46365b34eff323
Oksana Pak <okspak7@gmail.com>
2021-12-28 13:05:23 +0200

    Add id to img in js

--- js/plant-cards.js
+++ js/plant-cards.js
@@ -8,34 +8,35 @@ const cards = [
     {
         title: "Indoor Trees",
         description: "It's a beautiful and original option for a large room. A small indoor tree will add some zest to the office or a large living room. Large dracaena, ficus, alocasia will be a great option for your space.",
-        image: "img/our-plants-pictures/indoor-trees.webp"
+        image: "img/our-plants-pictures/indoor-trees.webp",
+        image_id: "plants_indortrees"
     },
     {
         title: "Plants in Florarium",
         description: "These are neatly planted compositions of succulents, grassroots, decorative ferns. Plants are selected so that they are in perfect harmony. Order your unique set of plants!",
-        image: "img/our-plants-pictures/plants-in-florarium.webp"
+        image: "img/our-plants-pictures/plants-in-florarium.webp",
+        image_id: "plants_inflorarium"
     },
     {
         title: "Big Ornamental Plants",
         description: "Charming pachypodium, yucca, monstera will become a stunning decoration of your room or office. These plants will make a great impression on your guests.",
-        image: "img/our-plants-pictures/big-ornamental-plants.webp"
+        image: "img/our-plants-pictures/big-ornamental-plants.webp",
+        image_id: "plants_bigornamental"
     }
 ];
-}
-)
 function renderCards(cards) {
-    let html = "";
+    const cardsContainer = document.querySelector('.plants_show');
     for (const card of cards) {
-        html +=
+        cardsContainer.innerHTML +=
         `<article class="plants_show_card">
         <div class="plants_show_container">
-            <img class="plants_show_pictures" src="${card.image}" alt="${card.title}">
+            <img id="${card.image_id}" class="plants_show_pictures" src="${card.image}" alt="${card.title}">
         </div>
         <h3 class="plants_show_name">${card.title}</h3>
         <p class="plants_show_description">${card.description}</p>
-    </article>`
+    </article>`;
     }
-    const cardsContainer = document.querySelector('.plants_show');
     cardsContainer.innerHTML = html;
 }
 renderCards(cards);
+})();
