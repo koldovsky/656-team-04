cb1b862a6e237409819b9062e38eb418f65b3ec7
Oksana Pak <okspak7@gmail.com>
2021-12-31 14:36:16 +0200

    add function JS to plant-cards

--- js/plant-cards.js
+++ js/plant-cards.js
@@ -1,33 +1,64 @@
-(function () {
+
+(async function () {
+
+    let category;
+
+    
 const cards = [
     {
         title: "Small Ornamental Plants",
         description: "Such plants will fit on any windowsill, decorate your home or work table. Chlorophytum, money plants, aglaonema are not demanding in care but very beautiful.",
-        image: "img/our-plants-pictures/small-ornamental-plants.webp"
+        image: "img/our-plants-pictures/small-ornamental-plants.webp",
+        category:"indoor"
     },
     {
         title: "Indoor Trees",
         description: "It's a beautiful and original option for a large room. A small indoor tree will add some zest to the office or a large living room. Large dracaena, ficus, alocasia will be a great option for your space.",
         image: "img/our-plants-pictures/indoor-trees.webp",
-        image_id: "plants_indortrees"
+        image_id: "plants_indortrees",
+        category:"indoor"
     },
     {
         title: "Plants in Florarium",
         description: "These are neatly planted compositions of succulents, grassroots, decorative ferns. Plants are selected so that they are in perfect harmony. Order your unique set of plants!",
         image: "img/our-plants-pictures/plants-in-florarium.webp",
-        image_id: "plants_inflorarium"
+        image_id: "plants_inflorarium",
+        category:"indoor"
     },
     {
         title: "Big Ornamental Plants",
         description: "Charming pachypodium, yucca, monstera will become a stunning decoration of your room or office. These plants will make a great impression on your guests.",
         image: "img/our-plants-pictures/big-ornamental-plants.webp",
-        image_id: "plants_bigornamental"
+        image_id: "plants_bigornamental",
+        category:"indoor"
+    },
+    {
+        title: "Outdoor Flowers",
+        description: "Flowers form the basis of the exterior and are the first to catch the eye. Charming anemones, freesia, lilies will provide brightness and a pleasant aroma. Choose your outdoor flower! ",
+        image: "img/our-plants-pictures/outdoor-flowers.webp",
+        image_id: "plants_outdoorflowers",
+        category:"outdoor"
     }
 ];
-function renderCards(cards) {
+
+function renderCards(cards, category) {
+
+    const filteredCards = cards.filter((card) => {
+        if (category) {
+          return category === card.category;
+        } else {
+          return true;
+        }
+    }); 
+
+    
     const cardsContainer = document.querySelector('.plants_show');
-    for (const card of cards) {
-        cardsContainer.innerHTML +=
+    let html = "";
+    for (const card of filteredCards)
+    {
+     
+   
+        html +=
         `<article class="plants_show_card">
         <div class="plants_show_container">
             <img id="${card.image_id}" class="plants_show_pictures" src="${card.image}" alt="${card.title}">
@@ -35,7 +66,31 @@ function renderCards(cards) {
         <h3 class="plants_show_name">${card.title}</h3>
         <p class="plants_show_description">${card.description}</p>
     </article>`;
+        
+    }
+     cardsContainer.innerHTML = html;
+}
+renderCards(cards, category = "indoor");
+
+
+
+function removeSelectedFilterClass() {
+    const buttons = document.querySelectorAll(".plants_location_button");
+    for (const button of buttons) {
+        button.classList.remove('selected');
     }
 }
-renderCards(cards);
-})();
+document.querySelector(".indoor_btm").addEventListener("click", (ev) => {
+    removeSelectedFilterClass();
+    ev.target.classList.add('selected');
+    category = "indoor";
+    renderCards(cards, category);
+  });
+  document.querySelector(".outdoor_btm").addEventListener("click", (ev) => {
+    removeSelectedFilterClass();
+    ev.target.classList.add('selected');  
+    category = "outdoor";
+    renderCards(cards, category);
+  });
+}
+)();
