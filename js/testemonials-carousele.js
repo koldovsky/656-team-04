6862d0fbe3b94ee6eddc8c5f75c5ed128187ea87
Marta <mart.panasyuk@gmail.com>
2022-01-09 13:42:54 +0000

    added new styles for testemonials

--- js/testemonials-carousele.js
+++ js/testemonials-carousele.js
@@ -1,21 +1,21 @@
-(function () {
+(function(){
 
     const slides = [
-        `   <div class="image-wrapper">
-                <img class="plant-icon" src="img/testemonials/green-line.webp" alt="Green-line>            
-            </div>`,
-        `   <div class="image-wrapper">
-                <img class="plant-icon" src="img/testemonials/green-wave.webp" alt="Green wave">
-            </div>`,
-        `   <div class="image-wrapper">
-                <img class="plant-icon" src="img/testemonials/home-and-garden.webp" alt="Home and Garden">
-            </div>`,
-        `   <div class="image-wrapper">
-                <img class="plant-icon" src="img/testemonials/home.webp" alt="Home">
-            </div>`,
-        `   <div class="image-wrapper">
-                <img class="plant-icon" src="img/testemonials/planta.webp" alt="Planta">
-            </div>`
+        `<div class="image-wrapper">
+            <img class="plant-icon" src="img/testemonials/green-line.webp" alt="Green-line">
+        </div>`,
+        `<div class="image-wrapper">
+            <img class="plant-icon" src="img/testemonials/green-wave.webp" alt="Green wave">
+        </div>`,
+        `<div class="image-wrapper">
+            <img class="plant-icon" src="img/testemonials/home-and-garden.webp" alt="Home and Garden">
+        </div>`,
+        `<div class="image-wrapper">
+            <img class="plant-icon" src="img/testemonials/home.webp" alt="Home">
+        </div>`,
+        `<div class="image-wrapper">
+            <img class="plant-icon" src="img/testemonials/planta.webp" alt="Planta">
+        </div>`
     ];
 
     let currentSlide = 0;
@@ -27,22 +27,18 @@
             const secondSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
             slideContainer.innerHTML += slides[secondSlide];
             if (window.innerWidth > 900) {
-                const thirdSlide = secondSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+                const thirdSlide = secondSlide + 1 >= slides.length ? 0 : secondSlide + 1;
                 slideContainer.innerHTML += slides[thirdSlide];
             }
         }
-
     }
 
     function nextSlide() {
-        currentSlide++;
-        if (currentSlide + 1 >= slides.length) currentSlide = 0;
+        currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1; 
         showCurrentSlide();
     }
 
     function prevSlide() {
-        currentSlide--;
-        if (currentSlide - 1 <= 0) currentSlide = slides.length - 1;
         currentSlide = currentSlide - 1 <= 0 ? slides.length - 1 : currentSlide - 1;
         showCurrentSlide();
     }
@@ -51,7 +47,6 @@
 
     document.querySelector('.testemonial_showforward.carousel-button').addEventListener('click', nextSlide);
     document.querySelector('.testemonial_showback.carousel-button').addEventListener('click', prevSlide);
-    window.addEventListener('resize', showCurrentSlide)
-
+    window.addEventListener('resize', showCurrentSlide);
 
 })();
