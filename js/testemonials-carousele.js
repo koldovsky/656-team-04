cf173621c83a1665615eedcf0ddd96e0ae40d535
Marta <mart.panasyuk@gmail.com>
2022-01-09 09:55:53 +0000

    added styles for testimonoals

--- js/testemonials-carousele.js
+++ js/testemonials-carousele.js
@@ -1,56 +1,46 @@
 (function () {
 
     const slides = [
-        `<article class="card">
-            <div class="wrapper">
+        `   <div class="image-wrapper">
                 <img class="plant-icon" src="img/testemonials/green-line.webp" alt="Green-line>            
-            </div>
-        </article>`,
-        `<article class="card">
-            <div class="wrapper">
+            </div>`,
+        `   <div class="image-wrapper">
                 <img class="plant-icon" src="img/testemonials/green-wave.webp" alt="Green wave">
-            </div>
-        </article>`,
-        `<article class="card">
-            <div class="wrapper">
+            </div>`,
+        `   <div class="image-wrapper">
                 <img class="plant-icon" src="img/testemonials/home-and-garden.webp" alt="Home and Garden">
-            </div>
-        </article>`,
-        `<article class="card">
-            <div class="wrapper">
+            </div>`,
+        `   <div class="image-wrapper">
                 <img class="plant-icon" src="img/testemonials/home.webp" alt="Home">
-            </div>
-        </article>`,
-        `<article class="card">
-            <div class="wrapper">
+            </div>`,
+        `   <div class="image-wrapper">
                 <img class="plant-icon" src="img/testemonials/planta.webp" alt="Planta">
-            </div>
-        </article>`
+            </div>`
     ];
 
     let currentSlide = 0;
 
-    function showCurrentSlide(){
+    function showCurrentSlide() {
         const slideContainer = document.querySelector('.testemonial-carousel_list');
         slideContainer.innerHTML = slides[currentSlide];
-        if(window.innerWidth > 600){ 
-           const secondSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
-           slideContainer.innerHTML += slides[secondSlide];
-            if(window.innerWidth > 900){
+        if (window.innerWidth > 600) {
+            const secondSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+            slideContainer.innerHTML += slides[secondSlide];
+            if (window.innerWidth > 900) {
                 const thirdSlide = secondSlide + 1 >= slides.length ? 0 : currentSlide + 1;
                 slideContainer.innerHTML += slides[thirdSlide];
-            }   
+            }
         }
 
     }
 
-   function nextSlide() {
+    function nextSlide() {
         currentSlide++;
         if (currentSlide + 1 >= slides.length) currentSlide = 0;
         showCurrentSlide();
     }
 
-    function prevSlide() {  
+    function prevSlide() {
         currentSlide--;
         if (currentSlide - 1 <= 0) currentSlide = slides.length - 1;
         currentSlide = currentSlide - 1 <= 0 ? slides.length - 1 : currentSlide - 1;
@@ -62,6 +52,6 @@
     document.querySelector('.testemonial_showforward.carousel-button').addEventListener('click', nextSlide);
     document.querySelector('.testemonial_showback.carousel-button').addEventListener('click', prevSlide);
     window.addEventListener('resize', showCurrentSlide)
-    
+
 
 })();
